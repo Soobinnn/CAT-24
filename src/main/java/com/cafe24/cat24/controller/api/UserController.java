c324d8fc8516173473c65e879ec341374427d3e3
Soobinnn <isb9082@naver.com>
2019-07-18 11:15:32 +0900

    회원가입(join) Controller Test

--- src/main/java/com/cafe24/cat24/controller/api/UserController.java
+++ src/main/java/com/cafe24/cat24/controller/api/UserController.java
@@ -1,11 +1,47 @@
 package com.cafe24.cat24.controller.api;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cafe24.cat24.dto.JSONResult;
+import com.cafe24.cat24.vo.UserVo;
+
 @RestController("userAPIController")
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController 
 {
 	
+	@RequestMapping(value="/list",method = RequestMethod.GET)
+	public String List(Model model)
+	{
+		return "user/helloworld";
+	}
+	
+	/** 회원가입 **/
+	@PostMapping(value="/join")
+	public ResponseEntity<JSONResult> join(@RequestBody @Valid UserVo userVo, BindingResult result) 
+	{
+		if( result.hasErrors() ) 
+		{
+			List<ObjectError> list = result.getAllErrors();
+			for(ObjectError error : list) 
+			{
+				return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(JSONResult.fail(error.getDefaultMessage())); 
+			}
+		}
+		
+		return ResponseEntity.status(HttpStatus.OK).body(JSONResult.success(null)); 
+	}
 }
--- src/test/java/com/cafe24/cat24/controller/api/UserControllerTest.java
+++ src/test/java/com/cafe24/cat24/controller/api/UserControllerTest.java
@@ -1,16 +1,29 @@
 package com.cafe24.cat24.controller.api;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.cafe24.config.web.TestWebConfig;
+import com.cafe24.cat24.vo.UserVo;
+import com.google.gson.Gson;
 import com.cafe24.cat24.config.AppConfig;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -31,5 +44,85 @@ public class UserControllerTest
 			build();
 	}
 	
+	@Test	
+	public void testHomePage() throws Exception
+	{
+		ResultActions resultActions = mockMvc.perform(get("/api/users/list"))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
 	
+	@Test
+	public void 회원가입테스트() throws Exception 
+	{
+		UserVo userVo = new UserVo();
+		
+		//1. 일반 회원가입 테스트
+		userVo.setName("임수빈");
+		userVo.setEmail("isb9082@gmail.com");
+		userVo.setPassword( "dlatnqls1!test" );
+		userVo.setGender( "NONE" );
+		
+		ResultActions resultActions =
+		mockMvc
+			.perform(post("/api/users/join")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new Gson().toJson(userVo)));
+	
+		resultActions
+		.andDo(print())
+		.andExpect(status().isOk());
+		
+		//2. 이름예외처리 테스트 : length must be between 2 and 8
+		userVo.setName("수");
+		userVo.setEmail("isb9082@gmail.com");
+		userVo.setPassword( "isb90822019!" );
+		userVo.setGender( "MALE" );
+		
+		resultActions =
+		mockMvc
+			.perform(post("/api/users/join")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new Gson().toJson(userVo)));
+	
+		resultActions
+		.andDo(print())
+		.andExpect(status().isBadRequest())
+		.andExpect(jsonPath("$.result", is("fail")));
+
+		
+		//2. 패스워드 예외처리 테스트 : 
+		userVo.setName("임수빈");
+		userVo.setEmail("isb9082@gmail.com");
+		userVo.setPassword( "isb90822019" );
+		userVo.setGender( "MALE" );
+		
+		resultActions =
+		mockMvc
+			.perform(post("/api/users/join")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new Gson().toJson(userVo)));
+	
+		resultActions
+		.andExpect(status().isBadRequest())
+		.andDo(print())
+		.andExpect(jsonPath("$.result", is("fail")));
+
+		//3. 성별 예외처리 테스트 : 
+		userVo.setName("임수빈");
+		userVo.setEmail("kickscar@gmail.com");
+		userVo.setPassword( "isb90822019!" );
+		userVo.setGender( "s" );
+		
+		resultActions =
+		mockMvc
+			.perform(post("/api/users/join")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(new Gson().toJson(userVo)));
+	
+		resultActions
+		.andExpect(status().isBadRequest())
+		.andDo(print())
+		.andExpect(jsonPath("$.result", is("fail")));
+	}
 }
