f50735aae32d9ca9cbced6e657fb7f7dfef92a91
Soobinnn <isb9082@naver.com>
2019-08-08 10:21:04 +0900

    메인페이지

--- CAT-24-Frontend/src/main/java/com/cafe24/cat24frontend/controller/MainController.java
+++ CAT-24-Frontend/src/main/java/com/cafe24/cat24frontend/controller/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class mainController 
+public class MainController 
 {
 	/** 메인 페이지 **/
 	@RequestMapping("/")
